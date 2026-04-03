3ad6effe0fe780fbf5f50547aae79e1eb63f3709
IamBobo <abdulsamad.arilewola@gmail.com>
2026-04-03 09:43:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Nuta/">
       <Routes>
+        <Route path="/Nuta/" element={<Index />} />
+        {/* Future routes like /connect and /push will go here */}
       </Routes>
     </BrowserRouter>
   );
